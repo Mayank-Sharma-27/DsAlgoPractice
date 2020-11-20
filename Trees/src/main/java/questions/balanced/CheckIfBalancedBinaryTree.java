f9197fb1bcc19bcaf8befcc6a114cac49fc15dff
Mayank-Sharma-27 <mayanksharma.sharma77@gmail.com>
2020-11-20 21:48:35 +0530

    Check if balanced tree

---
+++ Trees/src/main/java/questions/balanced/CheckIfBalancedBinaryTree.java
@@ -0,0 +1,32 @@
+package questions.balanced;
+
+import questions.TreeNode;
+
+public class CheckIfBalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        int l = height(root.left);
+        int r = height(root.right);
+
+
+        if(l-r>1){
+            return false;
+        }
+        if(r-l>1){
+            return false;
+        }
+        return isBalanced(root.left)&& isBalanced(root.right);
+
+    }
+
+    int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int l  = height(root.left)+1;
+        int r = height(root.right) +1;
+        return Math.max(l,r);
+    }
+}
