a716915e2b97dcf361e9576a3834195400c46862
Mayank-Sharma-27 <mayanksharma.sharma77@gmail.com>
2020-11-23 10:51:11 +0530

    Insert Into Binary Search Trees

---
+++ Trees/src/main/java/questions/insert/InsertIntoBinarySearchTree.java
@@ -0,0 +1,44 @@
+package questions.insert;
+
+import questions.TreeNode;
+
+public class InsertIntoBinarySearchTree {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            root = new TreeNode(val, null, null);
+            return root;
+        }
+        insert(root, val);
+        return root;
+    }
+
+    void insert(TreeNode root, int val) {
+        if (root == null) {
+            root = new TreeNode(val,null,null);
+            return;
+        }
+        if (root.val > val) {
+            if (root.left == null) {
+                TreeNode newNode = new TreeNode(val,null,null);
+                root.left = newNode;
+                return;
+            }
+        } else {
+            if (root.right == null) {
+                TreeNode newNode = new TreeNode(val,null,null);
+                root.right = newNode;
+                return;
+            }
+        }
+
+        if (root.val <= val) {
+            insert(root.right, val);
+        }
+        if (root.val > val) {
+            insert(root.left, val);
+        }
+        return;
+    }
+
+}
